87cd13c24f832c3e53ac606849c27a97d8db40f3
Ionut <Anca.Io.Catalin@student.utcluj.ro>
2024-10-16 15:41:00 +0300

    A

---
+++ main.py
@@ -0,0 +1,12 @@
+ny_text = ("Europa este un continent situat în întregime în emisfera nordică și "
+           "în cea mai mare parte în emisfera estică.")
+
+
+inceput = 0
+final = len(ny_text)
+pas = 1
+
+print(ny_text[inceput:final:pas])
+
+
+
